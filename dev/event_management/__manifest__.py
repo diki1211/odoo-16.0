a63d1c002df7b23802c6a35345b70f5626e75e86
diki1211 <46553135+diki1211@users.noreply.github.com>
2024-08-15 08:43:49 +0700

    final

--- dev/event_management/__manifest__.py
+++ dev/event_management/__manifest__.py
@@ -1,24 +1,3 @@
-# -*- coding: utf-8 -*-
-###################################################################################
-#
-#    Cybrosys Technologies Pvt. Ltd.
-#    Copyright (C) 2022-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
-#    Author: Avinash Nk(<avinash@cybrosys.in>)
-#
-#    This program is free software: you can modify
-#    it under the terms of the GNU Affero General Public License (AGPL) as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
-###################################################################################
 {
     'name': 'Catering Management',
     'version': '16.0.2.0.0',
